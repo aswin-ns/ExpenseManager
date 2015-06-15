c7377785a514e6a216f2c68334f1523bb852c3e7
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-15 19:03:08 +0530

    Add timeseries for plotting

--- app/src/main/java/info/androidhive/tabsswipe/Graphing.java
+++ app/src/main/java/info/androidhive/tabsswipe/Graphing.java
@@ -3,11 +3,20 @@ package info.androidhive.tabsswipe;
 import android.content.Context;
 import android.content.Intent;
 
+import org.achartengine.model.TimeSeries;
+
 /**
  * Created by ajaleelp on 15/6/15.
  */
 public class Graphing {
     public Intent getIntent(Context context){
+        int[] x = {1,2,3,4,5};
+        int[] y = {1,4,3,7,3};
+
+        TimeSeries series = new TimeSeries("Line1");
+        for (int i=0; i<x.length; i++){
+            series.add(x[i],y[i]);
+        }
         return null;
     }
 }
