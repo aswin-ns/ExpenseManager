3e18450f7f48d64af45ea7a4932a2a3128cf2555
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-15 17:11:56 +0530

    Add blank Graphing class with empty getIntent

---
+++ app/src/main/java/info/androidhive/tabsswipe/Graphing.java
@@ -0,0 +1,13 @@
+package info.androidhive.tabsswipe;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by ajaleelp on 15/6/15.
+ */
+public class Graphing {
+    public Intent getIntent(Context context){
+        return null;
+    }
+}
