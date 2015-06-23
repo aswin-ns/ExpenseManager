07383aa15b3dd4af0645ab4b82c9948550a2e6ae
aswin-ns <aswin.n.sudarsan@gmail.com>
2015-06-23 15:47:41 +0530

    Date Picker Added

--- app/build/generated/source/r/debug/info/androidhive/tabsswipe/R.java
+++ app/build/generated/source/r/debug/info/androidhive/tabsswipe/R.java
@@ -26,17 +26,17 @@ public final class R {
         public static final int usernew=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080033;
-        public static final int balance_trt=0x7f08002c;
-        public static final int balance_trt_1=0x7f080022;
-        public static final int btn_add_debt=0x7f08002e;
-        public static final int btn_add_debt_1=0x7f080024;
+        public static final int action_settings=0x7f080034;
+        public static final int balance_trt=0x7f08002d;
+        public static final int balance_trt_1=0x7f080023;
+        public static final int btn_add_debt=0x7f08002f;
+        public static final int btn_add_debt_1=0x7f080025;
         public static final int btn_authenticate_login=0x7f08000f;
         public static final int btn_authenticate_login_register=0x7f080015;
         public static final int btn_pie=0x7f080019;
         public static final int btn_type=0x7f080003;
-        public static final int btn_view_summary=0x7f08002f;
-        public static final int btn_view_summary_1=0x7f080025;
+        public static final int btn_view_summary=0x7f080030;
+        public static final int btn_view_summary_1=0x7f080026;
         public static final int button3=0x7f080006;
         public static final int button_enter=0x7f08000c;
         public static final int button_type_1=0x7f080008;
@@ -49,35 +49,36 @@ public final class R {
         public static final int edt_text_note=0x7f08000a;
         public static final int edt_usr_name=0x7f08000d;
         public static final int edt_usr_name_register=0x7f080013;
-        public static final int expense_trt=0x7f080028;
-        public static final int expense_trt_1=0x7f08001e;
+        public static final int expense_trt=0x7f080029;
+        public static final int expense_trt_1=0x7f08001f;
         public static final int imageView=0x7f080011;
         public static final int imageView_Register=0x7f080016;
-        public static final int img_trt=0x7f08002d;
-        public static final int img_trt_1=0x7f080023;
-        public static final int income_trt=0x7f08002b;
-        public static final int income_trt_1=0x7f080021;
-        public static final int label=0x7f080032;
-        public static final int logo=0x7f080031;
-        public static final int lst_expense=0x7f08001b;
-        public static final int lst_income=0x7f080030;
+        public static final int img_trt=0x7f08002e;
+        public static final int img_trt_1=0x7f080024;
+        public static final int income_trt=0x7f08002c;
+        public static final int income_trt_1=0x7f080022;
+        public static final int label=0x7f080033;
+        public static final int logo=0x7f080032;
+        public static final int lst_expense=0x7f08001c;
+        public static final int lst_income=0x7f080031;
         public static final int pager=0x7f080012;
-        public static final int relativeLayout=0x7f080026;
-        public static final int relativeLayout1=0x7f08001c;
+        public static final int relativeLayout=0x7f080027;
+        public static final int relativeLayout1=0x7f08001d;
         public static final int relativeLayout2=0x7f080000;
         public static final int relativeLayout3=0x7f080001;
         public static final int relativeLayout4=0x7f080007;
         public static final int tab2pager=0x7f080018;
-        public static final int textView=0x7f080027;
-        public static final int textView1=0x7f08001d;
+        public static final int textView=0x7f080028;
+        public static final int textView1=0x7f08001e;
         public static final int textView2=0x7f080005;
-        public static final int textView3=0x7f080029;
-        public static final int textView3_1=0x7f08001f;
+        public static final int textView3=0x7f08002a;
+        public static final int textView3_1=0x7f080020;
         public static final int textView4=0x7f080010;
-        public static final int textView5=0x7f08002a;
-        public static final int textView5_1=0x7f080020;
+        public static final int textView5=0x7f08002b;
+        public static final int textView5_1=0x7f080021;
         public static final int textView6=0x7f080017;
         public static final int textView_date_current_1=0x7f08000b;
+        public static final int txt_date_summary=0x7f08001b;
     }
     public static final class layout {
         public static final int activity_add_data=0x7f030000;
@@ -89,12 +90,14 @@ public final class R {
         public static final int activity_summary_screen=0x7f030006;
         public static final int activity_tab2=0x7f030007;
         public static final int activity_view_summary=0x7f030008;
-        public static final int expense_tab=0x7f030009;
-        public static final int fragment_next_month=0x7f03000a;
-        public static final int fragment_this_month=0x7f03000b;
-        public static final int income_tab=0x7f03000c;
-        public static final int list_item=0x7f03000d;
-        public static final int splash_screen=0x7f03000e;
+        public static final int activity_view_summary_expense=0x7f030009;
+        public static final int activity_view_summary_host=0x7f03000a;
+        public static final int expense_tab=0x7f03000b;
+        public static final int fragment_next_month=0x7f03000c;
+        public static final int fragment_this_month=0x7f03000d;
+        public static final int income_tab=0x7f03000e;
+        public static final int list_item=0x7f03000f;
+        public static final int splash_screen=0x7f030010;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -107,6 +110,8 @@ public final class R {
         public static final int menu_summary_screen=0x7f070007;
         public static final int menu_tab2=0x7f070008;
         public static final int menu_view_summary=0x7f070009;
+        public static final int menu_view_summary_expense=0x7f07000a;
+        public static final int menu_view_summary_host=0x7f07000b;
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
@@ -130,6 +135,8 @@ public final class R {
         public static final int title_activity_summary_screen=0x7f050012;
         public static final int title_activity_tab2=0x7f050013;
         public static final int title_activity_view_summary=0x7f050014;
+        public static final int title_activity_view_summary_expense=0x7f050015;
+        public static final int title_activity_view_summary_host=0x7f050016;
     }
     public static final class style {
         /** 
--- app/src/main/java/info/androidhive/expensemanager/adapter/ViewSummary.java
+++ app/src/main/java/info/androidhive/expensemanager/adapter/ViewSummary.java
@@ -1,6 +1,8 @@
 package info.androidhive.expensemanager.adapter;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +10,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.achartengine.GraphicalView;
 import org.achartengine.model.CategorySeries;
 
+import java.util.Calendar;
 import java.util.List;
 
 import info.androidhive.expensemanager.PieGraph;
@@ -23,11 +28,24 @@ public class ViewSummary extends Activity {
     Button piegraph;
     LinearLayout graphView;
 Button pie;
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
+    String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_summary);
+
+        dateView = (TextView) findViewById(R.id.txt_date_summary);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
 
         pie = (Button)findViewById(R.id.btn_pie);
         final DatabaseHandlerAddData db = new DatabaseHandlerAddData(this);
@@ -35,8 +53,8 @@ Button pie;
 
             @Override
             public void onClick(View view) {
-
-              List<SumByClass>a = db.getPie("6/2015");
+         date = dateView.getText().toString();
+              List<SumByClass>a = db.getPie(date);
                 String [] type = new String[a.size()];
                   int [] amnt = new int[a.size()];
                   int i = 0;
@@ -57,6 +75,38 @@ Button pie;
         });
 
 
+    }
+    public void OnShowDateNew(View v)
+    {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
+                .show();
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 
 
