75c5336873dd8aaa20ea43337e47fed1ec79b065
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-26 01:18:50 +0530

    Override default menu handlers to use super

--- app/src/main/java/info/androidhive/expensemanager/AddData.java
+++ app/src/main/java/info/androidhive/expensemanager/AddData.java
@@ -92,14 +92,6 @@ public class AddData extends BaseActivity {
         });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_data, menu);
-        return true;
-    }
-
     public void OnChangeDate(View v)
     {
         showDialog(999);
@@ -133,18 +125,13 @@ public class AddData extends BaseActivity {
                 .append(month).append("/").append(year));
     }
 
+    // use shared_menu from inherited BaseActivity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
